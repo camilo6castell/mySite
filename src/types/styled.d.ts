bb370f90635369940dc14a68fefcafa9a08ff9bc
Camilo Castellanos <kml107@hotmail.com>
2025-09-07 18:56:00 -0500

    version 2. Misssing new repository-data, types fixed, archives fixed two

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -2,7 +2,5 @@ import 'styled-components';
 import { ThemeColors } from '@/app/config/theme';
 
 declare module 'styled-components' {
-    export interface DefaultTheme extends ThemeColors {
-        mode: 'light' | 'dark'; // ejemplo, no rompe nada
-    }
+    export interface DefaultTheme extends ThemeColors { }
 }
